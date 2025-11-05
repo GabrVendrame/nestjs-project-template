004b4b13a81648a41cb000b712e2cff6bc7d4428
GabrVendrame <gabrielvendrame7@gmail.com>
2025-11-05 00:04:58 -0300

    chore: project setup

---
+++ README.md
@@ -0,0 +1,39 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
+</p>
+
+## Description
+
+Personal NestJs project starter template.
+
+## Project setup
+
+```bash
+$ npm install && npm prepare
+```
+
+## Compile and run the project
+
+```bash
+# development
+$ npm run start
+
+# watch mode
+$ npm run start:dev
+
+# production mode
+$ npm run start:prod
+```
+
+## Run tests
+
+```bash
+# unit tests
+$ npm run test
+
+# e2e tests
+$ npm run test:e2e
+
+# test coverage
+$ npm run test:cov
+```
---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import {
+    FastifyAdapter,
+    NestFastifyApplication,
+} from "@nestjs/platform-fastify";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { apiReference } from "@scalar/nestjs-api-reference";
+
+async function bootstrap() {
+    const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter(),
+    );
+
+    const documentBuilder = new DocumentBuilder()
+        .setTitle("")
+        .setDescription("")
+        .setVersion("")
+        .build();
+
+    const documentFactory = SwaggerModule.createDocument(app, documentBuilder);
+    /* use the commented code bellow to use swagger documentation */
+    // SwaggerModule.setup("api", app, documentFactory);
+
+    const scalarConfig = apiReference({
+        url: "/api-spec.json",
+        withFastify: true,
+        content: documentFactory,
+    });
+
+    app.use("/api", scalarConfig);
+
+    await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap();
